f2d64a946f0d65597b7ed9079c35a7373429c91d
王琳 <wanglin@wanglindeMacBook-Pro.local>
2022-04-13 10:51:11 +0800

    init vueX

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import rootModule from './rootModule'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+const files = require.context('./modules', false, /\.js$/)
+files.keys().forEach((key, index) => {
+  // 模块对应的内容
+  let store = files(key).default
+  let moduleName = key.replace(/^\.\//, '').replace(/\.js$/, '')
+  let modules = rootModule.modules = (rootModule.modules || {})
+  modules[moduleName] = store
+  modules[moduleName].namespaced = true
 })
+
+let store = new Vuex.Store(rootModule)
+export default store
---
+++ src/store/rootModule.js
@@ -0,0 +1,8 @@
+export default {
+  state: {
+    root: 'root'
+  },
+  mutations: {},
+  actions: {},
+  modules: {}
+}
